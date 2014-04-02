068b78fe541190d873a722d539aeb46be4af3397
shanika <shanikacj@gmail.com>
2014-04-02 14:27:08 +1300

    added new items

---
+++ ndt/elements.js
@@ -0,0 +1,195 @@
+Printer = draw2d.SetFigure.extend({
+    NAME: "Printer",
+
+    init : function()
+    {
+        this._super();
+        this.setDimension(128,128);
+    },
+
+    createSet: function(){
+        var set = this._super();
+        set.push( this.canvas.paper.image("img/printer.svg",0,0,128,128));
+	this.createPort('input', new draw2d.layout.locator.CenterLocator(this) );
+        return set;
+    }
+});
+
+
+
+Cloud = draw2d.SetFigure.extend({
+    NAME: "Printer",
+
+    init : function()
+    {
+        this._super();
+        this.setDimension(128,128);
+    },
+
+    createSet: function(){
+        var set = this._super();
+        set.push( this.canvas.paper.image("img/cloud.svg",0,0,128,128));
+	this.createPort('output', new draw2d.layout.locator.CenterLocator(this));
+        return set;
+    }
+});
+
+DesktopPC = draw2d.SetFigure.extend({
+    NAME: "Printer",
+
+    init : function()
+    {
+        this._super();
+        this.setDimension(128,128);
+    },
+
+    createSet: function(){
+        var set = this._super();
+        set.push( this.canvas.paper.image("img/desktop-pc.svg",0,0,128,128));
+	this.createPort('input', new draw2d.layout.locator.CenterLocator(this));
+        return set;
+    }
+});
+
+EFTPos = draw2d.SetFigure.extend({
+    NAME: "Printer",
+
+    init : function()
+    {
+        this._super();
+        this.setDimension(128,128);
+    },
+
+    createSet: function(){
+        var set = this._super();
+        set.push( this.canvas.paper.image("img/eftpos.svg",0,0,128,128));
+	this.createPort('input', new draw2d.layout.locator.CenterLocator(this));
+        return set;
+    }
+});
+
+InternetGlobe = draw2d.SetFigure.extend({
+    NAME: "Printer",
+
+    init : function()
+    {
+        this._super();
+        this.setDimension(128,128);
+    },
+
+    createSet: function(){
+        var set = this._super();
+        set.push( this.canvas.paper.image("img/internet-globe.svg",0,0,128,128));
+	this.createPort('output', new draw2d.layout.locator.CenterLocator(this));
+        return set;
+    }
+});
+
+Mako6500 = draw2d.SetFigure.extend({
+    NAME: "Printer",
+
+    init : function()
+    {
+        this._super();
+        this.setDimension(128,128);
+    },
+
+    createSet: function(){
+        var set = this._super();
+        set.push( this.canvas.paper.image("img/mako-6500.svg",0,0,128,128));
+	this.createPort('input', new draw2d.layout.locator.CenterLocator(this));
+	this.createPort('output', new draw2d.layout.locator.CenterLocator(this));
+        return set;
+    }
+});
+
+Mako7550 = draw2d.SetFigure.extend({
+    NAME: "Printer",
+
+    init : function()
+    {
+        this._super();
+        this.setDimension(128,128, new draw2d.layout.locator.CenterLocator(this));
+    },
+
+    createSet: function(){
+        var set = this._super();
+        set.push( this.canvas.paper.image("img/mako-7550.svg",0,0,128,128));
+	this.createPort('input', new draw2d.layout.locator.CenterLocator(this));
+	this.createPort('output', new draw2d.layout.locator.CenterLocator(this));
+        return set;
+    }
+});
+
+CMSServer = draw2d.SetFigure.extend({
+    NAME: "Printer",
+
+    init : function()
+    {
+        this._super();
+        this.setDimension(128,128);
+    },
+
+    createSet: function(){
+        var set = this._super();
+        set.push( this.canvas.paper.image("img/mako-cms-server.svg",0,0,128,128));
+	this.createPort('output', new draw2d.layout.locator.CenterLocator(this));
+        return set;
+    }
+});
+
+MobilePhone = draw2d.SetFigure.extend({
+    NAME: "Printer",
+
+    init : function()
+    {
+        this._super();
+        this.setDimension(128,128);
+    },
+
+    createSet: function(){
+        var set = this._super();
+        set.push( this.canvas.paper.image("img/mobile-phone.svg",0,0,128,128));
+	this.createPort('output', new draw2d.layout.locator.CenterLocator(this));
+        return set;
+    }
+});
+
+
+NetworkSwitch = draw2d.SetFigure.extend({
+    NAME: "Printer",
+
+    init : function()
+    {
+        this._super();
+        this.setDimension(128,128);
+    },
+
+    createSet: function(){
+        var set = this._super();
+        set.push( this.canvas.paper.image("img/network-switch.svg",0,0,128,128));
+	this.createPort('input', new draw2d.layout.locator.CenterLocator(this));
+	this.createPort('output', new draw2d.layout.locator.CenterLocator(this));
+        return set;
+    }
+});
+
+NotebookPC = draw2d.SetFigure.extend({
+    NAME: "Printer",
+
+    init : function()
+    {
+        this._super();
+        this.setDimension(128,128);
+    },
+
+    createSet: function(){
+        var set = this._super();
+        set.push( this.canvas.paper.image("img/notebook-pc.svg",0,0,128,128));
+	this.createPort('input', new draw2d.layout.locator.CenterLocator(this));
+        return set;
+    }
+});
+
+
+
--- ndt/ndt.js
+++ ndt/ndt.js
@@ -11,12 +11,41 @@ $(window).load(function () {
 	$.addRectangle = function(){
 		var rect = new draw2d.shape.basic.Rectangle();
 		rect.createPort('output');
+		rect.addCssClass('printer');
 		$.canvas.addFigure( rect,150,150);	
-	}
-	,
+	},
 	$.addPrinter = function () {
-		var printer = new draw2d.shape.widget.Widget();
-		// add Class
-		$.canvas.addFigure(printer,150,150); 	
+		$.canvas.addFigure(new Printer(),150,150); 	
+	},
+	$.addCloud = function () {
+		$.canvas.addFigure(new Cloud(),150,150);
+	},
+	$.addDesttopPC = function () {
+		$.canvas.addFigure(new DesktopPC(),150,150);
+	},
+	$.addMako6500 = function () {
+		$.canvas.addFigure(new Mako6500(),150,150);
+	},
+	$.addFTPos = function () {
+		$.canvas.addFigure(new EFTPos(),150,150);
+	},
+	$.addInternetGlobe = function () {
+		$.canvas.addFigure(new InternetGlobe(),150,150);
+	},
+	$.addMako7550 = function () {
+		$.canvas.addFigure(new Mako7550(),150,150);
+	},
+	$.addCMSServer = function () {
+		$.canvas.addFigure(new CMSServer(),150,150);
+	},
+	$.addMobilePhone = function () {
+		$.canvas.addFigure(new MobilePhone(),150,150);
+	},
+	$.addNetworkSwitch = function () {
+		$.canvas.addFigure(new NetworkSwitch(),150,150);
+	},
+	$.addNotebookPC = function () {
+		$.canvas.addFigure(new NotebookPC(),150,150);
 	}
 })(jQuery);
+
